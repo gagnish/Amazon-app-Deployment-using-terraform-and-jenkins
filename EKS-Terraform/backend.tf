e9a5827cb3245d31f45b4a097ea68602883a5c2d
gagnish <gdhiman@SEA.CORP.EXPECN.COM>
2024-06-11 23:35:53 +0530

    new code code1

--- EKS-Terraform/backend.tf
+++
@@ -1,7 +0,0 @@
-terraform {
-  backend "s3" {
-    bucket = "terrabucket22" # Replace with your actual S3 bucket name
-    key    = "EKSterraform/terraform.tfstate"
-    region = "us-east-1"
-  }
-}
